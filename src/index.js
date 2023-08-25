2cff6c573d73335555486fab1c5ab51fd167eaa1
catOWO2011 <catOWO2011@gmail.com>
2023-08-25 18:18:27 -0400

    Added App and Meal component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div>
+      <h1>Hi Cat!</h1>
+      <Meal />
+    </div>
+  );
+}
+
+function Meal() {
+  return (
+    <div>
+      <img
+        src="https://www.themealdb.com/images/media/meals/ytuvwr1503070420.jpg"
+        alt="mealImage"
+      />
+      <h1>Toad In The Hole</h1>
+      <p>
+        {`Preheat the oven to 200°C/fan180°C/gas 6. fry sausages in a non-stick
+        pan until browned.\r\nDrizzle vegetable oil in a 30cm x 25cm x 6cm deep
+        roasting tray and heat in the oven for 5 minutes.\r\nPut the plain flour
+        in a bowl, crack in the medium free-range eggs, then stir in the grated
+        horseradish. Gradually beat in the semi-skimmed milk. Season.\r\nPut the
+        sausages into the hot roasting tray and pour over the batter. Top with
+        cherry tomatoes on the vine and cook for 30 minutes until puffed and
+        golden.`}
+      </p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
