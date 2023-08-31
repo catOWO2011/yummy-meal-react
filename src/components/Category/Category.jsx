f2c124df6786b9cf24bef38a9af51df2d90835a5
catOWO2011 <catOWO2011@gmail.com>
2023-08-31 14:12:22 -0400

    Created and styled CategoryBoard, Category components

---
+++ src/components/Category/Category.jsx
@@ -0,0 +1,22 @@
+import "./Category.css";
+import wrinkledRedBackground from "./../../assets/img/wrinkled-red.avif";
+import kitchenClothWoodBackground from "./../../assets/img/kitchen-cloth-wood-table.avif";
+
+export default function Category({ id, name, imgUrl }) {
+  return (
+    <div
+      className="category-container"
+      id={`C${id}`}
+      style={{
+        backgroundImage: `url(${
+          id % 2 ? wrinkledRedBackground : kitchenClothWoodBackground
+        })`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+      }}
+    >
+      <img src={imgUrl} alt={name} />
+      <p className="overlay">{name}</p>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import "./index.css";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import beefImage from "./assets/img/beef.png";
+import CategoryBoard from "./components/CategoryBoard/CategoryBoard";
 
 function App() {
   return (
@@ -17,21 +20,7 @@ function App() {
 function Meal() {
   return (
     <div>
-      <img
-        src="https://www.themealdb.com/images/media/meals/ytuvwr1503070420.jpg"
-        alt="mealImage"
-      />
-      <h1>Toad In The Hole</h1>
-      <p style={{ whiteSpace: "pre-wrap" }}>
-        {`Preheat the oven to 200°C/fan180°C/gas 6. fry sausages in a non-stick
-        pan until browned.\r\nDrizzle vegetable oil in a 30cm x 25cm x 6cm deep
-        roasting tray and heat in the oven for 5 minutes.\r\nPut the plain flour
-        in a bowl, crack in the medium free-range eggs, then stir in the grated
-        horseradish. Gradually beat in the semi-skimmed milk. Season.\r\nPut the
-        sausages into the hot roasting tray and pour over the batter. Top with
-        cherry tomatoes on the vine and cook for 30 minutes until puffed and
-        golden.`}
-      </p>
+      <CategoryBoard beefImage={beefImage} />
     </div>
   );
 }
